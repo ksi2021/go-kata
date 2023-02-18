dd0ecaa3d588a5a5fcb403bca6c6ad177d4d0a9b
master <serj.cononenko2012@mail.ru>
2023-02-18 13:52:58 +0300

    add functions/defer/homework/main.go task #5.5.2

--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module github.com/ksi2021/go-kata
 
 go 1.19
 
-require github.com/ksi2021/greet v0.0.0-20230216124835-3c44ec59cb6f // indirect
+require github.com/ksi2021/greet v0.0.0-20230216124835-3c44ec59cb6f
---
+++ module2/functions/defer/homework/main.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+)
+
+// MergeDictsJob is a job to merge dictionaries into a single dictionary.
+type MergeDictsJob struct {
+	Dicts      []map[string]string
+	Merged     map[string]string
+	IsFinished bool
+}
+
+// errors
+var (
+	errNotEnoughDicts = errors.New("at least 2 dictionaries are required")
+	errNilDict        = errors.New("nil dictionary")
+)
+
+// BEGIN (write your solution here)
+func ExecuteMergeDictsJob(job *MergeDictsJob) (*MergeDictsJob, error) {
+
+	job.IsFinished = true
+
+	// проверка длины Dicts
+	if len(job.Dicts) < 2 {
+		return job, errNotEnoughDicts
+	}
+
+	// проверка на пустую мапу в Dicts
+	for i := range job.Dicts {
+		if job.Dicts[i] == nil {
+			return job, errNilDict
+		}
+	}
+
+	// добавление результатов в Merged
+	job.Merged = make(map[string]string)
+	for i := range job.Dicts {
+		for k, v := range job.Dicts[i] {
+			job.Merged[k] = v
+		}
+	}
+
+	return job, nil
+}
+
+// END
+
+// Пример работы
+func main() {
+	fmt.Println(ExecuteMergeDictsJob(&MergeDictsJob{}))                                                   // &MergeDictsJob{IsFinished: true}, "at least 2 dictionaries are required"
+	fmt.Println(ExecuteMergeDictsJob(&MergeDictsJob{Dicts: []map[string]string{{"a": "b"}, nil}}))        // &MergeDictsJob{IsFinished: true, Dicts: []map[string]string{{"a": "b"},nil}}, "nil dictionary"
+	fmt.Println(ExecuteMergeDictsJob(&MergeDictsJob{Dicts: []map[string]string{{"a": "b"}, {"b": "c"}}})) // &MergeDictsJob{IsFinished: true, Dicts: []map[string]string{{"a": "b", "b": "c"}}}, nil
+
+}
