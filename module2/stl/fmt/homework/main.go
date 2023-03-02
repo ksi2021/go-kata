cd7a8c1f1046e204eefdef3121710f19aeabecf7
master <serj.cononenko2012@mail.ru>
2023-03-02 13:24:30 +0300

    update stl\fmt\homework  100-1000 iterations

--- module2/stl/fmt/homework/main.go
+++ module2/stl/fmt/homework/main.go
@@ -2,12 +2,27 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
+
+	"github.com/brianvoe/gofakeit/v6"
 )
 
-func generateSelfStory(name string, age int, money float64) string {
-	return fmt.Sprintf("Hello! My name is %s. I`m %d y.o. And I also have $%.2f in my wallet fight now.", name, age, money)
+func generateSelfStory(p Person) string {
+	return fmt.Sprintf("Hello! My name is %s. I`m %d y.o. And I also have $%.2f in my wallet right now.", p.Name, p.Age, p.Money)
+}
+
+type Person struct {
+	Name  string  `fake:"{firstname}"`
+	Age   int     `fake:"{number:15,84}"`
+	Money float64 `fake:"{float64range:0,3000}"`
 }
 
 func main() {
-	fmt.Println(generateSelfStory("Vlad", 25, 10.0000025))
+	var p Person = Person{}
+	count := 100 + rand.Intn(1000-100+1)
+
+	for i := 0; i < count; i++ {
+		gofakeit.Struct(&p)
+		fmt.Println(generateSelfStory(p))
+	}
 }
